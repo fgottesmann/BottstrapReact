5c63dc3bbb287f132b2815e422f657980ce23fa8
fgottesmann <f.gottesmann@outlook.com>
2022-03-15 10:49:12 +0100

    Testaufgabe 2022.1

---
+++ src/app/Pages/MainPage.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import MainPage from './MainPage';
+
+export default {
+  title: 'Pages/MainPage',
+  component: MainPage,
+};
+
+export const Main = (): JSX.Element => <MainPage />;
