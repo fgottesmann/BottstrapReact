e50d9d80da07a9ad5cd7e07f6022e4a06c00028d
fgottesmann <f.gottesmann@outlook.com>
2022-03-15 10:51:38 +0100

    Testaufgabe 2022.2

---
+++ src/app/components/Carousel/Carousel.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Carousels from './Carousel';
+
+export default {
+  title: 'Components/Carousels',
+  component: Carousels,
+};
+
+export const Carousel = (): JSX.Element => <Carousels />;
